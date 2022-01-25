78595fff13615888e84c217d3125c5090d9ac468
ArunaachalamArun2000 <61001539+ArunaachalamArun2000@users.noreply.github.com>
2022-01-25 19:58:30 +0530

    first commit

---
+++ src/test/java/com/example/welcomedemo/WelcomedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welcomedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
